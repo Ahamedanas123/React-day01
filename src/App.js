8b749fb611528ae9e3dc36bb74c350d9573c6d25
Ahamedanas123 <ahamedanasali0@gmail.com>
2023-03-10 00:04:25 +0530

    first  commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
+    <div className="app1">
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Details
+        head="free"
+        symbol="$"
+        amount="0/month"
+        user="✔ single user"
+        storage="✔ 5GB storage"
+        project="✔ unlimited public projects"
+        access="✔ community access"
+        private="❌ Unlimited Private Projects"
+        phone="❌ Dedicated Phone Support"
+        subdomain="❌ Free Subdomain"
+        report="❌ Monthly Status Reports"
+        button="BUTTON"
+       
+      />
+
+      <Details
+        head="plus"
+        symbol="$"
+        amount="9/month"
+        user="✔ 5 user"
+        storage="✔ 50GB storage"
+        project="✔ unlimited public projects"
+        access="✔ community access"
+        private="✔ Unlimited Private Projects"
+        phone="✔ Dedicated Phone Support"
+        subdomain="✔ Free Subdomain"
+        report="❌ Monthly Status Reports"
+        button="BUTTON"
+      />
+
+      <Details
+        head="pro"
+        symbol="$"
+        amount="49/month"
+        user="✔ unlimited user"
+        storage="✔ 150GB storage"
+        project="✔ unlimited public project"        
+        access="✔ community access"
+        private="✔ Unlimited Private Projects"
+        phone="✔ Dedicated Phone Support"
+        subdomain="✔ unlimited Free Subdomain"
+        report="✔ Monthly Status Reports"
+        button="BUTTON"
+      />
+    </div>
     </div>
   );
 }
 
-export default App;
+function Details(props) {
+  return (
+    <div className="card">
+      <h1>{props.head}</h1>
+      <h3><span>{props.symbol}</span>
+      {props.amount}
+      </h3>
+      <hr />
+      <p>{props.user}</p>
+      <p>{props.storage}</p>
+      <p>{props.project}</p>
+      <p>{props.access}</p>
+      <p>{props.private}</p>
+      <p>{props.phone}</p>
+      <p>{props.sundomain}</p>
+      <p>{props.report}</p>
+      <h5>{props.button}</h5>
+      </div>
+  );
+}
